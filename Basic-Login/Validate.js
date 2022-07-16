7f339c4abf3e8e4a8d631e6029aded2645ee68da
Kushagra Sharma <98047633+2115500082@users.noreply.github.com>
2022-07-16 10:59:29 +0530

    Add files via upload

---
+++ Basic-Login/Validate.js
@@ -0,0 +1,13 @@
+function validate() 
+{ 
+var x = document.getElementById("Name").value; 
+var y =document.getElementById("Pass").value;
+if (x == "Web" && y == "Technology") 
+{ 
+alert("Welcome User"); 
+} 
+else 
+{
+alert("You are not an Authorised User"); 
+} 
+}
